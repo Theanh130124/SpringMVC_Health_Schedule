42c29c1864d31c320a70a67e899e0cbb75a2b510
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-08 01:46:02 +0700

    Update setup final

--- SpringMVCHeath_Schedule/src/main/java/com/trantheanh1301/configs/WebAppContextConfigs.java
+++ SpringMVCHeath_Schedule/src/main/java/com/trantheanh1301/configs/WebAppContextConfigs.java
@@ -1,47 +1,47 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package com.trantheanh1301.configs;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.format.FormatterRegistry;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//
-//
-//@Configuration
-//@EnableWebMvc
-//@EnableTransactionManagement
-//@ComponentScan(basePackages = {
-//    "com.trantheanh1301.controllers",
-//    "com.trantheanh1301.repository",
-//    "com.trantheanh1301.service"
-//})
-//public class WebAppContextConfigs implements WebMvcConfigurer{
-//    
-//    @Override
-//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//        configurer.enable();
-//    }
-//        @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-//    }
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trantheanh1301.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+    "com.trantheanh1301.controllers",
+    "com.trantheanh1301.repository",
+    "com.trantheanh1301.service"
+})
+public class WebAppContextConfigs implements WebMvcConfigurer{
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+        @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
 //     @Override
 //    public void addFormatters(FormatterRegistry registry) {
-//        registry.addFormatter(new CategoryFormatter());
+//        registry.addFormatter(new -----());
 //    }
-//     @Bean
-//    public StandardServletMultipartResolver multipartResolver() {
-//        return new StandardServletMultipartResolver();
-//    }
-//    
-//}
+     @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+    
+}
