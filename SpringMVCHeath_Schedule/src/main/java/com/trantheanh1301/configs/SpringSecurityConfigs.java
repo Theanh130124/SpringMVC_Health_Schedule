c0313015596ec22e80ac93557ab9e6afe06e833c
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-07 15:38:00 +0700

    Update Configs and database

---
+++ SpringMVCHeath_Schedule/src/main/java/com/trantheanh1301/configs/SpringSecurityConfigs.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trantheanh1301.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author LAPTOP
+ */
+
+
+@Configuration
+
+public class SpringSecurityConfigs {
+    
+}
