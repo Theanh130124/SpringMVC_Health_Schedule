e665af568c2592c8f4cc5cd0c1325792fda844d4
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-09 23:48:38 +0700

    Done api register user and login admin

---
+++ SpringMVC_Health_Schedule/src/main/java/com/trantheanh1301/service/UserService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.trantheanh1301.service;
+
+import com.trantheanh1301.pojo.User;
+import java.util.Map;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public interface UserService extends UserDetailsService{
+    User getUserByUsername(String username);
+    User register(Map<String, String> params, MultipartFile avatar);
+    
+}
