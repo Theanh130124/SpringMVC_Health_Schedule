8c5334f43474deabc3d8479fc6119962b676cf6e
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-17 18:25:34 +0700

    Update JWT - don't fix api use Token

---
+++ SpringMVC_Health_Schedule-be/src/main/java/com/trantheanh1301/controllers/ApiUserController.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.trantheanh1301.controllers;
+
+import com.trantheanh1301.pojo.User;
+import com.trantheanh1301.service.UserService;
+import com.trantheanh1301.utils.JwtUtils;
+import jakarta.ws.rs.core.MediaType;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author LAPTOP
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiUserController {
+
+    @Autowired
+    private UserService userDetailsService;
+
+    //Try catch chỗ này cho nó hiện lỗi chi tiết là gì đỡ vào log check
+    @PostMapping(path = "/users", consumes = MediaType.MULTIPART_FORM_DATA)
+    // trả ? để in đc error
+    public ResponseEntity<?> register(@RequestParam Map<String, String> params,
+            @RequestParam(value = "avatar") MultipartFile avatar) {
+        try {
+            User u = this.userDetailsService.register(params, avatar);
+            return new ResponseEntity<>(u, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            Map<String, String> error = new HashMap<>();
+            error.put("error", "Đã xảy ra lỗi " + ex.getMessage());
+            return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    //lấy token
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User u) {
+        if (this.userDetailsService.authenticate(u.getUsername(), u.getPassword())) {
+            try {
+                String token = JwtUtils.generateToken(u.getUsername());
+                return ResponseEntity.ok().body(Collections.singletonMap("token", token));
+
+            } catch (Exception e) {
+                return ResponseEntity.status(500).body("Lỗi khi tạo JWT");
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Sai thông tin đăng nhập");
+
+    }
+
+    //Từ token đưa ra thông tin người dùng -> decoded
+    @RequestMapping("/secure/profile")
+    @ResponseBody
+    public ResponseEntity<User> getProfile(Principal principal) {
+        return new ResponseEntity<>(this.userDetailsService.getUserByUsername(principal.getName()), HttpStatus.OK);
+    }
+}
