ada97075b14fc5584cc00508aff1c8fc38cb3554
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-21 15:13:02 +0700

    Done fe home

--- health-schedule-fe/src/component/layout/Header.js
+++ health-schedule-fe/src/component/layout/Header.js
@@ -6,7 +6,7 @@ import "./Styles/Header.css";
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" variant="light" bg="light" className="custom-header">
-            <Container>
+            <Container className="p-0">
                 <Navbar.Brand as={Link} to="/" className="header-logo-link ">
 
                     <h2 className="logo-title">
@@ -21,37 +21,35 @@ const Header = () => {
                             <span>Đặt khám ngay</span></Link>
 
 
-                            <Link to="/review" className="nav-item-with-subtext nav-link ms-4 text-center">
+                        <Link to="/review" className="nav-item-with-subtext nav-link ms-4 text-center">
                             Xem đánh giá
                             <span>Đánh giá về những bác sĩ</span>
                         </Link>
-                            <NavDropdown
+                        <NavDropdown
                             title={
-                               
-                                <div className="ms-4 nav-item-with-subtext text-center">
+                                <div className="nav-item-with-subtext">
                                     Tìm bác sĩ
                                     <span>Tìm ngay...</span>
                                 </div>
                             }
                             id="collapsible-nav-dropdown"
-                            className="nav-item-dropdown" 
-                         >
+                            className="nav-item-dropdown ms-4"
+                        >
                             {/* Tìm bác sĩ ở đây */}
                             <NavDropdown.Item as={Link} to="/findDoctor">Tìm ngay...</NavDropdown.Item>
-                            
                         </NavDropdown>
-                        
-                       
+
+
                     </Nav>
                     <Nav className="header-auth ">
-                        
-                         <Button variant="outline-success" as={Link} to="/register" className="me-2 sign-in-btn">
+
+                        <Button variant="outline-success" as={Link} to="/register" className="me-2 sign-in-btn">
                             Đăng ký
                         </Button>
                         <Button variant="primary" as={Link} to="/login" className="log-in-btn">
                             Đăng nhập
                         </Button>
-                      
+
                     </Nav>
 
                 </Navbar.Collapse>
