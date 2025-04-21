ce9ed539f4cafff2259c3ab8fe5c0307f993f274
Shriek-356 <khangvskiss@gmail.com>
2025-04-22 03:43:41 +0700

    update review api

--- SpringMVC_Health_Schedule-be/src/main/java/com/trantheanh1301/repository/AppointmentRepository.java
+++ SpringMVC_Health_Schedule-be/src/main/java/com/trantheanh1301/repository/AppointmentRepository.java
@@ -12,4 +12,5 @@ import com.trantheanh1301.pojo.Appointment;
  */
 public interface AppointmentRepository {
     public Appointment addOrUpdat(Appointment a);
+    public Appointment getAppointmentById(int id);
 }
