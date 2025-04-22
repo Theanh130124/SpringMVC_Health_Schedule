c1ecf2c308c7fc71107a19bfd3591cbe4e458742
Theanh130124 <theanhtran13012004@gmail.com>
2025-04-23 00:47:25 +0700

    Update UI Register , (validate)

--- health-schedule-fe/src/configs/Apis.js
+++ health-schedule-fe/src/configs/Apis.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { cookies } from "react-cookie"
 
 const BASE_URL = 'http://localhost:8080/SpringMVC_Health_Schedule/api/'
 
@@ -6,10 +7,23 @@ const BASE_URL = 'http://localhost:8080/SpringMVC_Health_Schedule/api/'
 export const endpoint = {
     'users' : '/users',
     'doctor_license': '/doctor_license',
-    'doctor': '/doctor'
+    'doctor': '/doctor',
+    'register':'/users',
+    'current_user':'/secure/profile'
    
 }
+
+
 
 export default axios.create({
     baseURL : BASE_URL
 })
+
+// export const authApis = () => {
+//     return axios.create({
+//         baseURL: BASE_URL,
+//         headers: {
+//             'Authorization' : `Bearer ${cookies.load(token)}`
+//         }
+//     })
+// }
